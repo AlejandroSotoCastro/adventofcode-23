d7c2153829140ff9b3fb2b7c3dd82334262f4fad
Alejandro Soto Castro <alejandrosc.eng@gmail.com>
2023-12-06 11:11:41 +0100

    easy

---
+++ day6/day6_1.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+
+// const input = "./day6/testInput.txt";
+const input = "./day6/input.txt";
+
+fs.readFile(input, "utf8", (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    const acceleration = 1; // mm/ms
+    const [rawTime, rawDistance] = data.split(/\r?\n/);
+    const time = rawTime.split(": ")[1].trim().split(/\s+/).map(Number);
+    const distance = rawDistance.split(": ")[1].trim().split(/\s+/).map(Number);
+
+    // each race has a distance and a time Example: {distance: 1000, time: 10}
+    const races = time.map((t, i) => ({ distance: distance[i], time: t }));
+
+    const allPossibleWays = [];
+    races.forEach((race) => {
+        allPossibleWays.push(calculateRace(race));
+    });
+    // multiply all possible ways together
+    const totalWays = allPossibleWays.reduce((a, b) => a * b);
+    console.log(totalWays);
+
+    function calculateRace(race) {
+        let possibleWays = 0;
+        for (let pushTime = 1; pushTime < race.time; pushTime++) {
+            const speed = acceleration * pushTime;
+            const travelTime = race.time - pushTime;
+            const distance = speed * travelTime;
+            if (distance > race.distance) {
+                possibleWays++;
+            }
+        }
+        return possibleWays;
+    }
+});
